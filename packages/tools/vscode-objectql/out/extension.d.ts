2ba956f6cdcb175d34e476e3d63d7ddeec92ffe9
Jack Zhuang <9340100@qq.com>
2026-01-16 11:57:55 +0800

    feat: 添加数据网格和 AMIS 项目跟踪器页面，更新 CORS 支持

---
+++ packages/tools/vscode-objectql/out/extension.d.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+/**
+ * Extension activation function
+ * Called when VSCode activates the extension
+ */
+export declare function activate(context: vscode.ExtensionContext): void;
+/**
+ * Extension deactivation function
+ */
+export declare function deactivate(): void;
+//# sourceMappingURL=extension.d.ts.map
