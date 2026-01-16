aa38245a955e44c98e8d579aa9ae1a9ea6d9d55c
Jack Zhuang <9340100@qq.com>
2026-01-16 11:37:52 +0800

    Update Jest config paths and dev script command
    Adjusted moduleNameMapper paths in Jest configs for express-server, enterprise-erp, and project-tracker examples to point to the new packages directory structure. Also updated the 'dev' script in package.json to use 'objectql:tracker' instead of 'example:tracker'.

--- examples/integrations/express-server/jest.config.js
+++ examples/integrations/express-server/jest.config.js
@@ -3,12 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleNameMapper: {
-    '^@objectql/core$': '<rootDir>/../../foundation/core/src',
-    '^@objectql/types$': '<rootDir>/../../foundation/types/src',
-    '^@objectql/platform-node$': '<rootDir>/../../foundation/platform-node/src',
-    '^@objectql/driver-sql$': '<rootDir>/../../drivers/sql/src',
-    '^@objectql/driver-mongo$': '<rootDir>/../../drivers/mongo/src',
-    '^@objectql/server$': '<rootDir>/../../runtime/server/src',
   },
   transform: {
     '^.+\\.ts$': ['ts-jest', {
